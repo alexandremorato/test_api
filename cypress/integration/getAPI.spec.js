3159cf93bc05ef8251dadc03d6e87729f355fca4
Alexandre <web_doc@hotmail.com>
2021-08-29 22:37:11 -0300

    test: Forçar erro no teste

--- cypress/integration/getAPI.spec.js
+++ cypress/integration/getAPI.spec.js
@@ -5,7 +5,7 @@ describe('Automação de teste - API', () => {
   it('Consulta de autor', () => {
     cy.getAuthors().
       should(response => {
-        expect(response.status).to.be.equal(200)
+        expect(response.status).to.be.equal(201)
       })
   })
 })
